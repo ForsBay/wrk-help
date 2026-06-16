e22396c42c130cdb80a50fc0cfbcaa2ebc45fae0
ForsBay <75286323+ForsBay@users.noreply.github.com>
2026-06-16 07:04:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,30 @@
-const CACHE_NAME = 'work-hours-v1';
+const CACHE_NAME = 'work-hours-v2';
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         './',
         './index.html',
-        './manifest.json'
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
       ]);
     })
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
